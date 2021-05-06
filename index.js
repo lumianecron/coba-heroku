f65710ea961bc598f834e34b28daf7e44b158f6a
octa <octaviuskrist@gmail.com>
2021-05-06 14:10:20 +0700

    initial commit

--- index.js
+++ index.js
@@ -153,8 +153,10 @@ app.post("/api/midtrans-notif", async function (req, res) {
     return res.status(200).send("");
 });
 
-app.listen(3000, function () {
-    console.log('listening on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log(`listening on port ${port}`);
 })
 // =======================================================================
 
